6956a72ec7144c26bdcbf9dd90cdf87aa025fe89
fraggi <Marien2424>
2015-01-09 17:23:54 -0800

    Added sample app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # http-filter
 Filters all outgoing http requests using a provided callback
+
+## Manual tests
+Once finished, this should be published as a package to NPM but for now use this to see how it works:
+
+    npm install
+    node app.js
+
--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+/*
+ * Sample app using http-filter to log all outgoing calls
+ */
 var httpFilter = require('./index.js');
 
-httpFilter.init(function(req) {
-    console.log('Retrieving: ' + req.method + ' ' + req.path);
+httpFilter.init(function(req, host) {
+    console.log('Outgoing request: ' + req.method + ' ' + host);
 });
 
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
+request('http://www.google.com', function (error, resp) {
+    if (!error && resp.statusCode == 200) {
+        console.log('Got ' + resp.body.length + ' bytes');
     }
 });
